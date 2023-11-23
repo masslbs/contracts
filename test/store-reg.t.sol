4f707eb8b4ac8e7cbcac38bc0554a099040665b8
Martin Becze <martin@becze.org>
2023-11-23 19:44:49 +0000

    add test for delegation

--- test/store-reg.t.sol
+++ test/store-reg.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.21;
 import "forge-std/Test.sol";
 import "openzeppelin-contracts/contracts/token/ERC721/IERC721Receiver.sol";
 import "lib/delegatable-sol/contracts/enforcers/RevocationEnforcer.sol";
+import "lib/delegatable-sol/contracts/TypesAndDecoders.sol";
 import "forge-std/console.sol";
 import "../src/store-reg.sol";
 
@@ -79,6 +80,60 @@ contract StoreTest is Test {
         assertEq(testHashUpdate, store.storeRootHash(storeId));
     }
 
+    function sign (uint256 pk, bytes32 hash) public returns(bytes memory) {
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(pk, hash);
+        return abi.encodePacked(r, s, v);
+    }
+
+    function test_delegatedAccesControl() public {
+        bytes32 testHashUpdate = 0x5049705e4c047d2cfeb1050cffe847c85a8dbd96e7f129a3a1007920d9c61d9a;
+        uint256 storeId = 55;
+        uint256 delegatedPrivateKey = 0x000000000000000000000000000000000000000000000000ffffffffffffffff;
+        uint256 ownerPrivateKey = 0x9324b26f0c529a96f0bb9a2379b8275ff4f40853148387e67827ca0b7d167fa7;
+
+        address owner = vm.addr(ownerPrivateKey);
+        address delegate = vm.addr(delegatedPrivateKey);
+
+        // create delegation
+        Delegation memory toMintCap;
+        toMintCap.delegate = delegate;
+
+        // create a signed delegation
+        SignedDelegation memory toMintCapSigned = SignedDelegation(
+            toMintCap,
+            sign(ownerPrivateKey, store.getDelegationTypedDataHash(toMintCap))
+        );
+        SignedDelegation[] memory toMintCapSigned_a = new SignedDelegation[](1);
+        toMintCapSigned_a[0] = toMintCapSigned; 
+ 
+        // create an invocation
+        Transaction memory t = Transaction(address(store),
+                                            10000000,
+                                            abi.encodeCall(store.updateRootHash,
+                                                          (storeId, testHashUpdate)));
+        Invocation memory invoke = Invocation(t, toMintCapSigned_a);
+        Invocation[] memory invoke_a = new Invocation [](1);
+        invoke_a[0] = invoke;
+
+        Invocations memory invokes = Invocations(invoke_a, ReplayProtection(1, 0));
+ 
+        // create a signed invocation
+        SignedInvocation memory invoke_signed = SignedInvocation(
+            invokes,
+            sign(delegatedPrivateKey, store.getInvocationsTypedDataHash(invokes))
+        );
+
+        SignedInvocation[] memory invoke_signed_a = new SignedInvocation[](1);
+        invoke_signed_a[0] = invoke_signed;
+
+        // create the store
+        store.mintTo(owner, storeId, testHash);
+        vm.prank(delegate);
+        // invoke the delegation
+        store.invoke(invoke_signed_a);
+        assertEq(testHashUpdate, store.storeRootHash(storeId));
+    }
+
     function testSafeContractReceiver() public {
         Receiver receiver = new Receiver();
         store.mintTo(address(receiver), 4, testHash);
@@ -94,6 +149,7 @@ contract StoreTest is Test {
 
 
 }
+
 
 contract Receiver is IERC721Receiver {
     function onERC721Received(
