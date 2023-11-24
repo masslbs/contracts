7a2e51fe25bbe38349c6819ef26a984c8afc4980
Martin Becze <martin@becze.org>
2023-11-24 21:29:28 +0000

    added store-reg abi

--- flake.nix
+++ flake.nix
@@ -24,11 +24,16 @@
         forge script ./script/store-reg.s.sol:MyScript --fork-url http://localhost:8545 --broadcast --no-auto-detect --via-ir
       '';
 
+      update_abi = pkgs.writeShellScriptBin "updateABI" ''
+        forge build --no-auto-detect --via-ir --silent &&  ${pkgs.jq}/bin/jq '.abi' ./out/store-reg.sol/Store.json > ./store-reg-abi.json
+      '';
+
       buildInputs = with pkgs; [
         # smart contracct dependencies
         foundry-bin
         solc
         deploy_market
+        update_abi
       ];
     in {
       devShell = pkgs.mkShell {
